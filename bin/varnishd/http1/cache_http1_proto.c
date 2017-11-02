90ffc236f1021de3b5691239d88695bf59ebc4c2
Nils Goroll <nils.goroll@uplex.de>
2017-11-02 12:06:32 +0100

    HTTP1_Write: reduce assertion overkill

--- bin/varnishd/http1/cache_http1_proto.c
+++ bin/varnishd/http1/cache_http1_proto.c
@@ -481,8 +481,6 @@ http1_WrTxt(const struct worker *wrk, const txt *hh,
 {
 	unsigned u;
 
-	CHECK_OBJ_NOTNULL(wrk, WORKER_MAGIC);
-	AN(wrk);
 	AN(hh);
 	AN(hh->b);
 	AN(hh->e);
@@ -497,10 +495,8 @@ HTTP1_Write(const struct worker *w, const struct http *hp, const int *hf)
 {
 	unsigned u, l;
 
+	CHECK_OBJ_NOTNULL(w, WORKER_MAGIC);
 	assert(hf == HTTP1_Req || hf == HTTP1_Resp);
-	AN(hp->hd[hf[0]].b);
-	AN(hp->hd[hf[1]].b);
-	AN(hp->hd[hf[2]].b);
 	l = http1_WrTxt(w, &hp->hd[hf[0]], " ", 1);
 	l += http1_WrTxt(w, &hp->hd[hf[1]], " ", 1);
 	l += http1_WrTxt(w, &hp->hd[hf[2]], "\r\n", 2);
