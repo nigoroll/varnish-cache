e90fd908ffa8bf18bc9b14a857f8da8f826760f9
Geoff Simmons <geoff@uplex.de>
2019-08-21 14:33:45 +0200

    Add the .authority field to backend definitions.
    If the .via field is also set, then the value of .authority is set
    as the authority TLV in the PROXY header. This gives the "true"
    backend (usually the ssl-onloader) the opportunity to set the SNI
    (HostName field) from the TLV value, for the TLS handshake with the
    remote backend.
    
    This mandates that PROXYv2 is always used with a via backend (since
    only version 2 supports TLVs).
    
    If the value of .authority is the empty string, then the TLV is not
    sent. If .authority is not set for the backend, then fall back to
    .host_header, which itself may have been a fallback to .host. Note
    that if neither .authority nor .host_header is set, and .host is
    set to an IP address, then the IP address is forwarded as the SNI
    value, which is not permitted for HostName (RFC4366 ch 3.1). So
    users are advised to set either .authority of .host_header, or set
    .authority="", when .via is set.

--- include/vrt.h
+++ include/vrt.h
@@ -55,6 +55,8 @@
  * NEXT (2020-09-15)
  *	Added VRT_DirectorResolve()
  *	Added VCL_STRING VRT_BLOB_string(VRT_CTX, VCL_BLOB)
+ *	authority field added to struct vrt_backend
+ *
  * 11.0 (2020-03-16)
  *	Changed type of vsa_suckaddr_len from int to size_t
  *	New prefix_{ptr|len} fields in vrt_backend
@@ -379,6 +381,7 @@ extern const void * const vrt_magic_string_unset;
 	rigid char			*port;			\
 	rigid char			*path;			\
 	rigid char			*hosthdr;		\
+	rigid char			*authority;		\
 	vtim_dur			connect_timeout;	\
 	vtim_dur			first_byte_timeout;	\
 	vtim_dur			between_bytes_timeout;	\
@@ -395,6 +398,7 @@ extern const void * const vrt_magic_string_unset;
 		DA(port);			\
 		DA(path);			\
 		DA(hosthdr);			\
+		DA(authority);			\
 		DN(connect_timeout);		\
 		DN(first_byte_timeout);		\
 		DN(between_bytes_timeout);	\
