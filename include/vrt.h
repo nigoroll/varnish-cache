eb6e8ae2d4d387dc94e22fcd5c9c527329aa5b44
Nils Goroll <nils.goroll@uplex.de>
2020-04-09 13:35:47 +0200

    Add error handling to the VFP/VDP API
    There is no way for a vmod to determine if a filter name is already
    taken.

--- include/vrt.h
+++ include/vrt.h
@@ -73,6 +73,10 @@
  *	VRT_call() added
  *	VRT_check_call() added
  *	VRT_handled() added
+ *	VRT_AddVDP() signature changed for error handling
+ *	VRT_AddVFP() signature changed for error handling
+ *	VRT_RemoveVDP() signature changed for error handling
+ *	VRT_RemoveVFP() signature changed for error handling
  * 12.0 (2020-09-15)
  *	Added VRT_DirectorResolve()
  *	Added VCL_STRING VRT_BLOB_string(VRT_CTX, VCL_BLOB)
