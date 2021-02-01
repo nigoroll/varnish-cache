4595ecfb2606dcf2858972258074732f15419f64
Nils Goroll <nils.goroll@uplex.de>
2021-02-01 17:27:12 +0100

    Give VPI its private struct in the worker
    (struct vrt_ctx).handling is a special animal: As seen by VGC, struct
    vrt_ctx is const (and rightly so), yet VCL needs to have a way to
    communicate up to core code the return value from subs.
    
    So, handling was a (non-const) pointer into struct worker. This is all
    good and well, with the minor drawback that VMODs would get to see it,
    while we should make it very clear that they have to go through
    VRT_handling() / VRT_handled() and know the rules (no reset of
    handling).
    
    As we now want to move also the vpi count into "the ctx", before adding
    another unsigned pointer, we introduce struct wrk_vpi, provided by the
    worker, but owned by vpi.
    
    With this change, we thus formalize the vpi count requirement and also
    remove the handling variable from vmods' direct line of sight.
    
    This commit comprises the manual changes which either could not be or
    were not worth being automated.
    
    The next two commits contain a coccinelle script to do the rest of the
    work, and the reult of it.

--- bin/varnishd/cache/cache.h
+++ bin/varnishd/cache/cache.h
@@ -252,7 +252,8 @@ struct worker {
 
 	unsigned		cur_method;
 	unsigned		seen_methods;
-	unsigned		handling;
+
+	struct wrk_vpi		*vpi;
 };
 
 /* Stored object -----------------------------------------------------
